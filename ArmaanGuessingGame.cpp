eed06389e8a13fb4670f3597f77c4aab5942ba60
U-BSD\398121 <398121@SHS-DGDPCS2.beaverton.k12.or.us>
2022-09-08 14:23:34 -0700

    Initial Code

---
+++ ArmaanGuessingGame.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  bool guessed = false;
+  int randInt = srand(time(NULL));
+  
+  while (guessed == false) {
+    
+  }
+  return 0;
+}
