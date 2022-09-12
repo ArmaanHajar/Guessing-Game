5212e3fed5f065315d22a9bbfd3e062dbc8d6521
ArmaanHajar <armaanhajar@icloud.com>
2022-09-12 14:06:22 -0700

    first commit

--- ArmaanGuessingGame.cpp
+++ ArmaanGuessingGame.cpp
@@ -2,12 +2,53 @@
 
 using namespace std;
 
-int main()
-{
-  bool guessed = false;
+int main() {
+  bool newgame = false;
+  int guesses = 0;
   
-  while (guessed == false) {
+  while (newgame == false) {
+    bool guessed = false;
+    int range = 100 - 0 + 1;
+    int randNum = rand() % range + 0;
+    int input = 0;
+
+    cout << randNum << endl;
+
+    cout << "Start Guessing Numbers" << endl;
+
+    while (guessed == false) { 
+      cin >> input;
+
+      if (input > randNum) {
+	cout << "Too High!" << endl;
+	guesses++;
+      }
+
+      else if (input < randNum) {
+	cout << "Too Low!" << endl;
+	guesses++;
+      }
     
+      if (input == randNum) {
+	guesses++;
+	cout << "Well Done, It Took You " << guesses << " Guesses to get The Answer!" << endl;
+
+	char YorN;
+	cout << "Do you want to play again? (Y/N)" << endl;
+	cin >> YorN;
+
+	if (YorN == 'Y') {
+	  guesses = 0;
+	  cout << "Starting New Game!" << endl;
+	  guessed = true;
+	}
+	if (YorN == 'N') {
+	  cout << "Thank You For Playing!" << endl;
+	  newgame = true;
+	  guessed = true;
+	}
+      }
+    }
+    return 0;
   }
-  return 0;
 }
